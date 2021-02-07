225c8eb947bc321c0c6587121707904dd3f07d28
Lexie Barthelemess <78189568+lexiekateb@users.noreply.github.com>
2021-02-07 04:16:41 -0600

    Add files via upload

--- main_menu_state.lua
+++ main_menu_state.lua
@@ -1,12 +1,10 @@
-local gameState = require 'game_state'
+local lobbyState = require 'lobby_state'
 
 return {
     new = function()
         return {
             buttons = {
-                "new game",
-                "load game",
-                "options",
+                "play",
                 "quit",
             },
             cursor = 1,
@@ -17,10 +15,11 @@ return {
                     if button == "down" and buttonState == "pressed" then state.cursor = (state.cursor % #state.buttons) + 1
                     elseif button == "up" and buttonState == "pressed" then state.cursor = ((state.cursor - 2) % #state.buttons) + 1
                     elseif button == "accept" and buttonState == "pressed" then
-                        if state.buttons[state.cursor] == "new game" then return gameState.new() end
+                        if state.buttons[state.cursor] == "play" then return lobbyState.new() end
                     end
                 elseif event.type == "draw" then
                     local graphics = love.graphics
+                    graphics.clear(0, 0, 0, 1)
                     for i = 1, #state.buttons do
                         if i == state.cursor then graphics.setColor(0.95, 0.95, 0.95, 1)
                         else graphics.setColor(0.5, 0.5, 0.5, 1)
